2e945f81b71d2d9c31c0eb3f5d664ee303c48c58
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-13 17:22:36 +0900

    [Bronze V] Title: A/B, Time: 52 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            string[] str = Console.ReadLine().Split();
+            Console.WriteLine(double.Parse(str[0]) / double.Parse(str[1]));
+        }
+    }
+}
