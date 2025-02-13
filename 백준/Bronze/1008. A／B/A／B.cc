1426fa59dbddb004a24f3a1100ff166112607408
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-13 16:07:08 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    double num1 = 0.0;
+    double num2 = 0.0;
+    cin >> num1 >> num2;
+    cout.precision(15);
+    cout << fixed;
+    cout << num1 / num2;
+    return 0;
+}
