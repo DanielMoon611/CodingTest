b56dd3350edbd5cfd04cb6b5068713ac4407859a
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-13 15:24:47 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            string[] str = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
+        }
+    }
+}
