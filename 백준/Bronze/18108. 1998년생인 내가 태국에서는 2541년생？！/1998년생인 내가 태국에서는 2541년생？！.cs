a49c01d1ccafeb9e28939f37658116bae04412fe
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-14 13:33:52 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(y - 543);
+        }
+    }
+}
