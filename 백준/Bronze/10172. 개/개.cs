777903a9b69bad20b7ab42e7dfeb6f16a0ddb53c
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-14 13:45:55 +0900

    [Bronze V] Title: 개, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+        }
+    }
+}
