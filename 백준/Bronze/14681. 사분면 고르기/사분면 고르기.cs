efcbde382f3e364972dde545bfefabad3ad0ceb3
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-14 14:11:12 +0900

    [Bronze V] Title: 사분면 고르기, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            if(X > 0) {
+                if(Y > 0) {
+                    Console.WriteLine("1");   
+                } else {
+                    Console.WriteLine("4");
+                }
+            } else {
+                if(Y > 0) {
+                    Console.WriteLine("2");
+                } else {
+                    Console.WriteLine("3");
+                }
+            }
+        }
+    }
+}
