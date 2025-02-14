ff845390f8862cc307666e58ea48d1efbbb63afb
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-14 14:09:01 +0900

    [Bronze V] Title: 윤년, Time: 52 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            int year = int.Parse(Console.ReadLine());
+            if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+        }
+    }
+}
