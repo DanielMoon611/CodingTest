5efa5f20b3f6f563efda314c547a95661f54f028
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-14 13:39:47 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long A = 0;
+    long B = 0;
+    long C = 0;
+    cin >> A >> B >> C;
+    cout << A + B + C << endl;
+    return 0;
+}
