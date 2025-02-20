1284c6fed62b7126428f202012ef343a4630d7e6
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-20 17:25:36 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N = 0;
+    cin >> N;
+    N /= 4;
+    for (int i = 0; i < N; ++i)
+        cout << "long ";
+    cout << "int" << endl;
+    return 0;
+}
