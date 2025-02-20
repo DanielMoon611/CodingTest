397e97550c3c0f3bc26db177b2fffd7d447e4ae4
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-20 18:05:52 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 56 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Cstutorial {
+    class Pragram {
+        static void Main(string[] args) {
+            int N = int.Parse(Console.ReadLine());
+            N /= 4;
+            for (int i = 0; i < N; ++i)
+                Console.WriteLine("long");
+            Console.WriteLine("int");
+        }
+    }
+}
