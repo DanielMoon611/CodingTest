20ef3572a9131731f789a84124e609eec5df3400
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-21 17:39:36 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T = 0;
+    int i = 1;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> T;
+    while (true) {
+        if (i > T)
+            break;
+        int a, b = 0;
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a + b << '\n';
+        ++i;
+    }
+    return 0;
+}
