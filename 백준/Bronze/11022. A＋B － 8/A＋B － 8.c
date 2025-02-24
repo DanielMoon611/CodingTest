691013b0d0bb226adf9b22349f1691519142f52d
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-02-24 15:39:32 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    int T = 0;
+    int i = 1;
+    scanf("%d", &T);
+    while (true) {
+        if (i > T)
+            break;
+        int a, b = 0;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+        ++i;
+    }
+    return 0;
+}
