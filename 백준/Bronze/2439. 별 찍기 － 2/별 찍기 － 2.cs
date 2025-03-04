a82faf9ef597f053cfbdacacbd67790829101767
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-03-04 15:35:35 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 352 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j)
+                    if (i + j >= N - 1)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
