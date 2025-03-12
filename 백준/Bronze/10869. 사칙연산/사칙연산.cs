369c755a6ff5b366691717eae2679d199d345072
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 08:59:14 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5516 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cs
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -1,14 +1,12 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] str = Console.ReadLine().Split();
-            Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
-            Console.WriteLine(int.Parse(str[0]) - int.Parse(str[1]));
-            Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
-            Console.WriteLine(int.Parse(str[0]) / int.Parse(str[1]));
-            Console.WriteLine(int.Parse(str[0]) % int.Parse(str[1]));
-        }
+class Program {
+    static void Main(string[] args) {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
+        Console.WriteLine(int.Parse(str[0]) - int.Parse(str[1]));
+        Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
+        Console.WriteLine(int.Parse(str[0]) / int.Parse(str[1]));
+        Console.WriteLine(int.Parse(str[0]) % int.Parse(str[1]));
     }
 }
