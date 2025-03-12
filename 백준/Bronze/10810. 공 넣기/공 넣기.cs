efca75f4669b689215bbbc99925ecbeb84858c22
Daniel Moon <80665627+DanielMoon611@users.noreply.github.com>
2025-03-12 15:08:02 +0900

    [Bronze III] Title: 공 넣기, Time: 52 ms, Memory: 6080 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+    static void Main() {
+        string[] inputs = Console.ReadLine().Split();
+        int N = int.Parse(inputs[0]);
+        int M = int.Parse(inputs[1]);
+        
+        int[] arr = new int[N];
+        
+        for (int index = 0; index < M; index++) {
+            string[] command = Console.ReadLine().Split();
+            int i = int.Parse(command[0]);
+            int j = int.Parse(command[1]);
+            int k = int.Parse(command[2]);
+
+            for (int x = i - 1; x < j; x++)
+                arr[x] = k;
+        }
+        
+        Console.WriteLine(string.Join(" ", arr));
+    }
+}
