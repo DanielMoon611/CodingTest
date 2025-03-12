b9dfcd3aaebbd559eb8cad2306b327b21d274770
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 08:57:46 +0900

    [Bronze V] Title: A/B, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cs
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -1,10 +1,8 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] str = Console.ReadLine().Split();
-            Console.WriteLine(double.Parse(str[0]) / double.Parse(str[1]));
-        }
+class Program {
+    static void Main(string[] args) {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(double.Parse(str[0]) / double.Parse(str[1]));
     }
 }
