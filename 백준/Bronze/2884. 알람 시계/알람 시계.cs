593e8ca808d06f57214a3930d8c390080d1f3f93
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:02:44 +0900

    [Bronze III] Title: 알람 시계, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cs
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cs
@@ -1,25 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] str = Console.ReadLine().Split();
-            int h = int.Parse(str[0]);
-            int m = int.Parse(str[1]);
-            if(m >= 45)
-                m -= 45;
-            else {
-                if(h == 0)
-                    h = h - 1 + 24;
-                else
-                    h -= 1;
-                m = m - 45 + 60;
-            }
-            Console.WriteLine("{0} {1}", h, m);
+class Program {
+    static void Main(string[] args) {
+        string[] str = Console.ReadLine().Split();
+        int h = int.Parse(str[0]);
+        int m = int.Parse(str[1]);
+        if(m >= 45)
+            m -= 45;
+        else {
+            if(h == 0)
+                h = h - 1 + 24;
+            else
+                h -= 1;
+            m = m - 45 + 60;
         }
+        Console.WriteLine("{0} {1}", h, m);
     }
 }
