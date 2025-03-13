71ab451a5712b8e934419ed28508b162c56d14fa
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:28:39 +0900

    [Bronze V] Title: A+B - 7, Time: 52 ms, Memory: 5728 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -1,15 +1,13 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            int T = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= T; ++i) {
-                string[] str = Console.ReadLine().Split();
-                int a = int.Parse(str[0]);
-                int b = int.Parse(str[1]);
-                Console.WriteLine("Case #{0}: {1}", i, a + b);
-            }
+class Program {
+    static void Main(string[] args) {
+        int T = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= T; ++i) {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+            Console.WriteLine("Case #{0}: {1}", i, a + b);
         }
     }
 }
