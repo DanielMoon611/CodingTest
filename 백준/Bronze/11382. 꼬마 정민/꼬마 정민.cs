61a00435ff42828f2f2becdb1b44bcdc08ec24de
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:40:51 +0900

    [Bronze V] Title: 꼬마 정민, Time: 52 ms, Memory: 5492 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -4,6 +4,5 @@ class Program {
     static void Main(string[] args) {
         string[] str = Console.ReadLine().Split();
         Console.WriteLine(long.Parse(str[0]) + long.Parse(str[1]) + long.Parse(str[2]));
-        
     }
 }
