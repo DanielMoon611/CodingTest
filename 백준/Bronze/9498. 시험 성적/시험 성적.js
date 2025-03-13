56ac875055200e048903381778de73a8281b449a
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:50:53 +0900

    [Bronze V] Title: 시험 성적, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const score = parseInt(input);
+
+switch (Math.floor(score / 10)) {
+    case 10:
+    case 9:
+        console.log("A");
+        break;
+    case 8:
+        console.log("B");
+        break;
+    case 7:
+        console.log("C");
+        break;
+    case 6:
+        console.log("D");
+        break;
+    default:
+        console.log("F");
+        break;
+}
