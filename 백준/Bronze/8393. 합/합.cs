6da3616b6ab259e0653e69f57ee2996778b16627
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:12:45 +0900

    [Bronze V] Title: 합, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.cs
+++ 백준/Bronze/8393. 합/합.cs
@@ -1,15 +1,13 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            int N = int.Parse(Console.ReadLine());
-            int answer = 0;
-            
-            for(int i = 1; i <= N; ++i)
-                answer += i;
-            
-            Console.WriteLine("{0}", answer);
-        }
+class Program {
+    static void Main(string[] args) {
+        int N = int.Parse(Console.ReadLine());
+        int answer = 0;
+        
+        for(int i = 1; i <= N; ++i)
+            answer += i;
+        
+        Console.WriteLine("{0}", answer);
     }
 }
