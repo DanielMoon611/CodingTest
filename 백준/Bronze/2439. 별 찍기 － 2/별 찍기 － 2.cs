a7c1bdc477c9226fbc119be96ae40a73341a4b13
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:38:38 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 336 ms, Memory: 6572 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -1,17 +1,16 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            int N = int.Parse(Console.ReadLine());
-            for (int i = 0; i < N; ++i) {
-                for (int j = 0; j < N; ++j)
-                    if (i + j >= N - 1)
-                        Console.Write("*");
-                    else
-                        Console.Write(" ");
-                Console.WriteLine();
+class Program {
+    static void Main(string[] args) {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (i + j >= N - 1)
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
             }
+            Console.WriteLine();
         }
     }
 }
