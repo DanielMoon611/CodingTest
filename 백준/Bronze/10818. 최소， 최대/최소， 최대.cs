8a82a771692d0e50ee297bce088ce4b40699a7ea
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:56:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 316 ms, Memory: 77124 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cs
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cs
@@ -1,18 +1,16 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            int N = int.Parse(Console.ReadLine());
-            string[] str = Console.ReadLine().Split();
-            int min = int.Parse(str[0]), max = int.Parse(str[0]);
-            for (int i = 0; i < N; ++i) {
-                if (int.Parse(str[i]) < min)
-                    min = int.Parse(str[i]);
-                if (int.Parse(str[i]) > max)
-                    max = int.Parse(str[i]);
-            }
-            Console.WriteLine("{0} {1}", min, max);
+class Program {
+    static void Main(string[] args) {
+        int N = int.Parse(Console.ReadLine());
+        string[] str = Console.ReadLine().Split();
+        int min = int.Parse(str[0]), max = int.Parse(str[0]);
+        for (int i = 0; i < N; ++i) {
+            if (int.Parse(str[i]) < min)
+                min = int.Parse(str[i]);
+            if (int.Parse(str[i]) > max)
+                max = int.Parse(str[i]);
         }
+        Console.WriteLine("{0} {1}", min, max);
     }
 }
