7617da15f0cc2ec56fae361f77a408347372fa2d
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:45:42 +0900

    [Bronze V] Title: 개수 세기, Time: 52 ms, Memory: 5780 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cs
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cs
@@ -1,16 +1,14 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            int N = int.Parse(Console.ReadLine());
-            string[] str = Console.ReadLine().Split();
-            int v = int.Parse(Console.ReadLine());
-            int count = 0;
-            for (int i = 0; i < N; ++i)
-                if (v == int.Parse(str[i]))
-                    count++;
-            Console.WriteLine(count);
-        }
+class Program {
+    static void Main(string[] args) {
+        int N = int.Parse(Console.ReadLine());
+        string[] str = Console.ReadLine().Split();
+        int v = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = 0; i < N; ++i)
+            if (v == int.Parse(str[i]))
+                count++;
+        Console.WriteLine(count);
     }
 }
