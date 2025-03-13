1fc7cdfed0d07f8d82f5bc4c0e53a9c6a4c5bd25
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:02:15 +0900

    [Bronze V] Title: 나머지, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.cs
+++ 백준/Bronze/10430. 나머지/나머지.cs
@@ -1,20 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] numStr = Console.ReadLine().Split();
-            int a = int.Parse(numStr[0]);
-            int b = int.Parse(numStr[1]);
-            int c = int.Parse(numStr[2]);
-            Console.WriteLine((a + b) % c);
-            Console.WriteLine(((a % c) + (b % c)) % c);
-            Console.WriteLine((a * b) % c);
-            Console.WriteLine(((a % c) * (b % c)) % c);
-        }
+class Program {
+    static void Main(string[] args) {
+        string[] numStr = Console.ReadLine().Split();
+        int a = int.Parse(numStr[0]);
+        int b = int.Parse(numStr[1]);
+        int c = int.Parse(numStr[2]);
+        Console.WriteLine((a + b) % c);
+        Console.WriteLine(((a % c) + (b % c)) % c);
+        Console.WriteLine((a * b) % c);
+        Console.WriteLine(((a % c) * (b % c)) % c);
     }
 }
