a69d5b4d938878661e24670174da97a564fe2476
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:46:09 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
@@ -1,19 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] str = Console.ReadLine().Split();
-            if(int.Parse(str[0]) > int.Parse(str[1]))
-                Console.WriteLine(">");
-            else if(int.Parse(str[0]) < int.Parse(str[1]))
-                Console.WriteLine("<");
-            else
-                Console.WriteLine("==");
-        }
+class Program {
+    static void Main(string[] args) {
+        string[] str = Console.ReadLine().Split();
+        if(int.Parse(str[0]) > int.Parse(str[1]))
+            Console.WriteLine(">");
+        else if(int.Parse(str[0]) < int.Parse(str[1]))
+            Console.WriteLine("<");
+        else
+            Console.WriteLine("==");
     }
 }
