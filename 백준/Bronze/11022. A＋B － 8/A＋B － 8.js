8bfb9567b862f1e3ff577ef420fe52099d58fe74
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:32:26 +0900

    [Bronze V] Title: A+B - 8, Time: 100 ms, Memory: 9536 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = parseInt(input[0]);
+
+for (let i = 1; i <= T; ++i) {
+    const [a, b] = input[i].split(' ').map(Number);
+    console.log(`Case #${i}: ${a} + ${b} = ${a + b}`);
+}
