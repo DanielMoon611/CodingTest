30fa38f7cf659b617109f6f9b47ae0c3b6db0b53
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:14:28 +0900

    [Bronze V] Title: A×B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.cs
+++ 백준/Bronze/10998. A×B/A×B.cs
@@ -4,6 +4,5 @@ class Program {
     static void Main(string[] args) {
         string[] str = Console.ReadLine().Split();
         Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
-        
     }
 }
