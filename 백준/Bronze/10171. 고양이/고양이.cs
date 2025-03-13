9f0743f5ab5a7d68477be5fdcef62b790c3edc7b
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:41:29 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.cs
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("\\    /\\");
-            Console.WriteLine(" )  ( ')");
-            Console.WriteLine("(  /  )");
-            Console.WriteLine(" \\(__)|");
-        }
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
     }
 }
