34409dc200510699778ff2149477d088819b519e
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:07:41 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -1,7 +1,4 @@
-require('fs').readFileSync('dev/stdin').toString()
 const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
 console.log(a+b);
 console.log(a-b);
 console.log(a*b);
