56956901b3d6ff956078acff3018da8410a74a16
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:07:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cs
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cs
@@ -1,28 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] str = Console.ReadLine().Split();
-            int H = int.Parse(str[0]);
-            int M = int.Parse(str[1]);
-            int C = int.Parse(str[2]);
-            int result = 0;
-            
-            if(H == M && M == C)
-                result = 10000 + H * 1000;
-            else if(H == M || H == C)
-                result = 1000 + H * 100;
-            else if(M == C)
-                result = 1000 + M * 100;
-            else
-                result = ((H > M ? H : M) > C ? (H > M ? H : M) : C) * 100;
-            
-            Console.WriteLine(result);
-        }
+class Program {
+    static void Main(string[] args) {
+        string[] str = Console.ReadLine().Split();
+        int H = int.Parse(str[0]);
+        int M = int.Parse(str[1]);
+        int C = int.Parse(str[2]);
+        int result = 0;
+        
+        if(H == M && M == C)
+            result = 10000 + H * 1000;
+        else if(H == M || H == C)
+            result = 1000 + H * 100;
+        else if(M == C)
+            result = 1000 + M * 100;
+        else
+            result = ((H > M ? H : M) > C ? (H > M ? H : M) : C) * 100;
+        
+        Console.WriteLine(result);
     }
 }
