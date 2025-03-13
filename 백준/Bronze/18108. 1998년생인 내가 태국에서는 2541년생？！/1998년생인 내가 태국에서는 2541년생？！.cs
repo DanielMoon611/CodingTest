a6c8ad6a8c6e133225d55fda7bedfa1e573898c1
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:20:52 +0900

    Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            int y = int.Parse(Console.ReadLine());
-            Console.WriteLine(y - 543);
-        }
+class Program {
+    static void Main(string[] args) {
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine(y - 543);
     }
 }
