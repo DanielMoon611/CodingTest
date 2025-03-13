081401c413b3d8bda93ce7f6a8f65a8bd69454b2
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:52:01 +0900

    [Bronze V] Title: X보다 작은 수, Time: 60 ms, Memory: 7152 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cs
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cs
@@ -1,18 +1,16 @@
 using System;
 using System.Text;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string[] str = Console.ReadLine().Split();
-            int N = int.Parse(str[0]);
-            int X = int.Parse(str[1]);
-            string[] nums = Console.ReadLine().Split();
-            StringBuilder answer = new StringBuilder();
-            for (int i = 0; i < N; ++i)
-                if(X > int.Parse(nums[i]))
-                    answer.Append(nums[i] + " ");
-            Console.WriteLine(answer.ToString());
-        }
+class Program {
+    static void Main(string[] args) {
+        string[] str = Console.ReadLine().Split();
+        int N = int.Parse(str[0]);
+        int X = int.Parse(str[1]);
+        string[] nums = Console.ReadLine().Split();
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < N; ++i)
+            if(X > int.Parse(nums[i]))
+                answer.Append(nums[i] + " ");
+        Console.WriteLine(answer.ToString());
     }
 }
