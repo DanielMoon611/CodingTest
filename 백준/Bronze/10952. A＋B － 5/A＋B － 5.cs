702996f40d45cc42e53f45252b2f5715ae806b3e
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:40:27 +0900

    [Bronze V] Title: A+B - 5, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
@@ -1,16 +1,14 @@
 using System;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            while (true) {
-                string[] str = Console.ReadLine().Split();
-                int a = int.Parse(str[0]);
-                int b = int.Parse(str[1]);
-                if(a == 0 && b == 0)
-                    break;
-                Console.WriteLine("{0}", a + b);
-            }
+class Program {
+    static void Main(string[] args) {
+        while (true) {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+            if(a == 0 && b == 0)
+                break;
+            Console.WriteLine("{0}", a + b);
         }
     }
 }
