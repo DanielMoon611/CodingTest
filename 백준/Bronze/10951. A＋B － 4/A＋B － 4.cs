f33eafb624b5b608e62081f396a49b6cbe4c5d1e
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 15:43:02 +0900

    [Bronze V] Title: A+B - 4, Time: 56 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cs
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cs
@@ -1,17 +1,12 @@
 using System;
 
-class Program
-{
-    static void Main()
-    {
+class Program {
+    static void Main(string[] args) {
         string input;
-        while ((input = Console.ReadLine()) != null)
-        {
+        while ((input = Console.ReadLine()) != null) {
             string[] numbers = input.Split();
             if (numbers.Length == 2 && int.TryParse(numbers[0], out int a) && int.TryParse(numbers[1], out int b))
-            {
                 Console.WriteLine(a + b);
-            }
         }
     }
 }
