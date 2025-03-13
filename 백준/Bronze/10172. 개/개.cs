c7684b7724a448bb7a1948c947a741648c0df304
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 14:43:45 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.cs
+++ 백준/Bronze/10172. 개/개.cs
@@ -1,17 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("|\\_/|");
-            Console.WriteLine("|q p|   /}");
-            Console.WriteLine("( 0 )\"\"\"\\");
-            Console.WriteLine("|\"^\"`    |");
-            Console.WriteLine("||_/=\\\\__|");
-        }
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
     }
 }
