42ab84b4ada5329fdd35559cbac137aa5e9adf88
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 13:52:03 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.cs
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CsTutorial {
-    class Program {
-        static void Main(string[] args) {
-            string str = Console.ReadLine();
-            Console.WriteLine(str + "??!");
-        }
+class Program {
+    static void Main(string[] args) {
+        string str = Console.ReadLine();
+        Console.WriteLine(str + "??!");
     }
 }
