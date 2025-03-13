bae5b68c0bdc53a35f5f6385d1da8838364b68f7
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-03-13 13:45:05 +0900

    [Bronze V] Title: A/B, Time: 92 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.js
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -1,5 +1,2 @@
-require('fs').readFileSync('dev/stdin').toString()
 const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
-console.log(a/b);
+console.log(Number(input[0]) / Number(input[1]));
