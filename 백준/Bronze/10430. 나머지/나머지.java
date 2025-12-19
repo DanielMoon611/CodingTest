f01bac2a40cac7c465e7b1acde47fd5ac11fa25c
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-12-19 16:17:38 +0900

    [Bronze V] Title: 나머지, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,18 @@
+// (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+// (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
