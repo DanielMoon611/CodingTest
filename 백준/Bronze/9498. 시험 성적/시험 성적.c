7775c2050ea05a8ae29e61a65afae07e4add7f79
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2025-12-20 21:01:57 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int score = 0;
+    scanf("%d", &score);
+    if(score >= 90)
+        printf("A");
+    else if(90 > score && score >= 80)
+        printf("B");
+    else if(80 > score && score >= 70)
+        printf("C");
+    else if(70 > score && score >= 60)
+        printf("D");
+    else
+        printf("F");
+    return 0;
+}
