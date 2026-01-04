351ac3ae73d0d867f639ddaf9fb8a90c0f696857
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2026-01-04 17:39:34 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int T = 0;
+    cin >> T;
+    for(int i = 0; i < T; ++i) {
+        int A = 0;
+        int B = 0;
+        cin >> A >> B;
+        cout << A + B << endl;
+    }
+}
