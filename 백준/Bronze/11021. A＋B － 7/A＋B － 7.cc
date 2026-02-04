ecd9b53c53867366ba3d0aa7a167ffcda1757679
lxxnxdlxnx <80665627+DanielMoon611@users.noreply.github.com>
2026-02-04 16:49:21 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int T = 0;
+    cin >> T;
+    for(int i = 0; i < T; ++i) {
+        int A, B = 0;
+        cin >> A >> B;
+        cout << "Case #" << i + 1 << ": " << A + B << endl;
+    }
+}
